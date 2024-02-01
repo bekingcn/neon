670f7b699df41289b14f63caf1815804090c5b27
Sasha Krassovsky <sasha@neon.tech>
2024-02-01 06:33:32 +0000

    Rename variable

--- compute_tools/src/compute.rs
+++ compute_tools/src/compute.rs
@@ -393,9 +393,10 @@ impl ComputeNode {
     // Gets the basebackup in a retry loop
     #[instrument(skip_all, fields(%lsn))]
     pub fn get_basebackup(&self, compute_state: &ComputeState, lsn: Lsn) -> Result<()> {
-        let mut timeout_ms = 500;
+        // With 7 attempts starting at 500ms, we'll have retries over the span of 63 sec total
+        let mut retry_period_ms = 500;
         let mut attempts = 0;
-        let max_attempts = 5;
+        let max_attempts = 7;
         loop {
             let result = self.try_get_basebackup(compute_state, lsn);
             match result {
@@ -407,8 +408,8 @@ impl ComputeNode {
                         "Failed to get basebackup: {} (attempt {}/{})",
                         e, attempts, max_attempts
                     );
-                    std::thread::sleep(std::time::Duration::from_millis(timeout_ms));
-                    timeout_ms *= 2;
+                    std::thread::sleep(std::time::Duration::from_millis(retry_period_ms));
+                    retry_period_ms *= 2;
                 }
                 Err(_) => {
                     return result;
